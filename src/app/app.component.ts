c64b052ee1e507d3378113f94036f3c64de2b198
Dan Kurin <dan@leapfinder.com>
2023-12-31 13:19:04 -0500

    Close Menu drawer on Nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterOutlet } from '@angular/router';
 import { SchemaService } from './services/schema.service';
 import { Observable } from 'rxjs';
 import { OpenAPIV2 } from 'openapi-types';
 import { LetDirective } from '@ngrx/component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LetDirective],
+  imports: [
+    CommonModule, 
+    RouterOutlet, 
+    LetDirective,
+    FormsModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  public drawerOpen: boolean = false;
   title = 'frontend';
   public menuItems$: Observable<OpenAPIV2.DefinitionsObject | undefined>;
   constructor(
@@ -26,6 +33,7 @@ export class AppComponent {
 
   public navigate(key: string) {
     this.router.navigate(['view', key]);
+    this.drawerOpen = false;
   }
 
   public getMenuKeys(menuItems: OpenAPIV2.DefinitionsObject | undefined) : string[] {
